26a42444eab610c80e81e8463cde39e78cce91a5
fraanab <91223447+fraanab@users.noreply.github.com>
2023-10-10 16:55:05 +0000

    last update

--- core/models.py
+++ core/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Messages(models.Model):
 	email = models.EmailField()
 	name = models.CharField(max_length=200)
+	discord = models.CharField(max_length=200, blank=True, null=True)
 	message = models.TextField(max_length=500)
 	date = models.DateTimeField(auto_now_add=True)
 
--- core/views.py
+++ core/views.py
@@ -12,21 +12,22 @@ def main(request):
 	return render(request, 'main.html')
 
 def contact(request):
-	if request.method == 'POST':
-		name = request.POST.get('name')
-		email = request.POST.get('email')
-		message = request.POST.get('message')
-		Messages.objects.create(name=name, email=email, message=message)
+  if request.method == 'POST':
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    discord = request.POST.get('discord')
+    message = request.POST.get('message')
+    Messages.objects.create(name=name, email=email, message=message)
 
-		try:
-			send_mail(
-				f'New message from {name}',
-				f'Email: {email}, Message: {message}',
-				settings.EMAIL_HOST_USER,
-				[f'fraanab.oml@gmail.com'],
-				fail_silently=False,
-				)
-		except Exception as e:
-			messages.error(request, f'Error sending message {str(e)}')
-		return JsonResponse({'message': "Message sent successfully!", "style":"color: #0c0"})
-	return render(request, 'contact.html')
+    try:
+      send_mail(
+        f'New message from {name}',
+        f'Email: {email}, Message: {message}, DC: {discord}',
+        settings.EMAIL_HOST_USER,
+        ['fraanab.oml@gmail.com'],
+        fail_silently=False,
+      )
+    except Exception as e:
+      messages.error(request, f'Error sending message {str(e)}')
+    return JsonResponse({'message': "Message sent successfully!", "style":"color: #0c0"})
+  return render(request, 'contact.html')
